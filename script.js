f5fcd7cae2cdd20e99beefd32352a4bd8a20a841
Marin NICOLLE <marinnicollep@gmail.com>
2026-06-27 14:00:11 +0200

    projet fonctionel, rajout par la suite d'element de fond

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const invocTerminee = document.getElementById("invocation_terminee")
 // LINK BUTTON
 
 const boutonDonjon = document.getElementById("bouton_donjon")
-const boutonInvoc = document.getElementsById("bouton_invoc")
-const boutonChanger = document.getElementsById("bouton_changer")
+const boutonInvoc = document.getElementById("bouton_invoc")
+const boutonChanger = document.getElementById("bouton_changer")
 
 // LINK INPUT
 
@@ -35,11 +35,11 @@ const descriptions = {
    
     "Goblin" : "Oscilliant entre perfidité maligne et nervosuté exacerbé, le goblin est un compagnon qui sera vous preter main forte au combat. A moins qu'il decide de s'enfuir face au danger...",
 
-    "Fee" : "Esprit de la forêt, protectrice des voyageurs égarés, elle saura vous guider grace à la limière qui la fait vivre, gare a ses fourberies qu'elle pourrait vous faire pour passer le temps.",
+    "Fée" : "Esprit de la forêt, protectrice des voyageurs égarés, elle saura vous guider grace à la limière qui la fait vivre, gare a ses fourberies qu'elle pourrait vous faire pour passer le temps.",
 
-    "Loup-Garou" : "Atteints de Lycantropie, la plus part des humains en étant affecté n'en ont même pas conscience et mènent une vie sanguinaire chaque nuit de pleine lune.",
+    "Loup-Garou" : "Atteints de Lycantropie, la plus part des humains en étant affecté n'en ont même pas conscience et mènent une vie sanguinaire chaque nuit de pleine lune. On devrait de te laisser tanquille maintenant",
 
-    "Sirene" : "Certains marisn pensent avoir fait une belle prise en les sortant de leurs filets, mais c'est plutôt elle qui vient d'en prendre une. J'espere qu'il y a un peu d'eau là ou vous allez..."
+    "Sirène" : "Certains marins pensent avoir fait une belle prise en les sortant de leurs filets, mais c'est plutôt elle qui vient d'en prendre une. J'espere qu'il y a un peu d'eau là ou vous allez..."
 
 }
 
@@ -63,27 +63,34 @@ const cacherCreature =() => {
 // Logique bouton
 
 boutonDonjon.addEventListener('click', function() {
-    cacher(entreeDonjon)
-    montrer(introDonjon)
+    hide(entreeDonjon)
+    show(introDonjon)
 })
 
 
 boutonInvoc.addEventListener('click', function() {
-   const prenom = inputPrenom.value.trim()
-   const creature = selectCreature.value.trim()
+   
+    const prenom = inputPrenom.value.trim()
+    const creature = selectCreature.value.trim()
 
     if (!prenom) {
-        alert("Je crois ne pas avoir entendu ton prénom, je suis un peu dur de la feuille, essaie en parlant un peu plus fort je t(en pris.")
+        alert("Je crois ne pas avoir entendu ton prénom, je suis un peu dur de la feuille, essaie en parlant un peu plus fort je t'en pris.")
         return
     }
     
     cacherCreature()
-    montrer(imagesCreatures[creature])
+    show(imagesCreatures[creature])
+    show(descriptCreat)
+
+    hide(introDonjon)
+    show(invocTerminee)
+
+    descriptCreat.textContent = `${prenom}, nous avons reussi à invoquer un.e ${creature} ! ${descriptions[creature]}`
 })
 
 // CHANGER DE FORME
 
 boutonChanger.addEventListener('click', function() {
-    cacher(invocTerminee)
-    montrer(introDonjon)
+    hide(invocTerminee)
+    show(introDonjon)
 })
